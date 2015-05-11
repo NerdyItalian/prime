c057f69d14b445813138430b2fb1760a7b0828f5
NerdyItalian <atava8384@gmail.com>
2015-05-10 22:01:40 -0500

    now with ajax calls and template.html

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,5 +1,39 @@
-console.log("Hey, I loaded!");
+var templateHTML;
+var experience;
+
+function getInfo (){
+    $.ajax({
+        url:'./assets/data/data.json',
+        dataType: 'json',
+        success: function(response){
+            experience = response;
+            addExperience();
+        }
+    });
+}
+
+function addExperience (){
+    var el = $('#otherInfo');
+    el.append(templateHTML);
+    el.children().children('#title').append(experience.info.title);
+    el.children().children('#extraOne').append(experience.info.extraOne);
+    el.children().children('#extraTwo').append(experience.info.extraTwo);
+    el.children().children('#extraThree').append(experience.info.extraThree);
+}
 
 $(document).ready(function(){
+    $(".moreExperience").on('click', function(){
+        if(!templateHTML){
+            $.ajax({
+                url: '/views/template.html',
+                success: function(response) {
+                    templateHTML = response;
+                    getInfo();
+                }
+            });
+        }
+    });
+});
 
-})
+
+
